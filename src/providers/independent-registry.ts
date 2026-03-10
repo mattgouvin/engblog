5df40beaa9c9fd8c846f64b4a20a1c70870fda6d
Matt Gouvin <mattgouvin@gmail.com>
2026-03-09 22:29:47 -0400

    Added 2 new independent sources

--- src/providers/independent-registry.ts
+++ src/providers/independent-registry.ts
@@ -4,6 +4,8 @@ import { RssProvider } from "./rss-provider";
 
 export const independentRegistry: Record<IndependentSource, DataProvider> = {
   [IndependentSource.LatentSpace]: new RssProvider("https://www.latent.space/feed.xml"),
+  [IndependentSource.LennysNewsletter]: new RssProvider("https://www.lennysnewsletter.com/feed.xml"),
+  [IndependentSource.RobinSloan]: new RssProvider("https://www.robinsloan.com/lab.xml"),
 };
 
 export const ALL_INDEPENDENT_SOURCES = Object.values(IndependentSource);
